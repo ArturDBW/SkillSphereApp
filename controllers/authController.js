2b6d57c3d79086180fcb74f51e22bff1a54442be
Artur Wiśniewski <artixbtw@gmail.com>
2024-02-01 14:49:00 +0100

    add roleAuthentication function

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,6 +55,21 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+// -------------------------------------------------------------> Autoryzacje!
+// do sprawdzenia jaka role posiada uzytkownik
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+
+    next();
+  };
+};
 
 // protect authorization
 
@@ -82,8 +97,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   const currentUser = await User.findById(decoded.id);
   if (!currentUser) {
     return next(
-      new AppError("The user belonging to this token does no longer exist"),
-      401
+      new AppError("The user belonging to this token does no longer exist", 401)
     );
   }
 
