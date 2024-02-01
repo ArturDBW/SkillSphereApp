97e56d0611cdcacbbc31e63eff692158f89e1101
Artur Wiśniewski <artixbtw@gmail.com>
2024-02-01 15:06:08 +0100

    add resetPassword function

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,6 +7,11 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
+router.patch(
+  "/updateMyPassword",
+  authController.protect,
+  authController.updatePassword
+);
 router.patch("/updateMe", authController.protect, userController.updateMe);
 router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
