937cc2dfd407879b96211a48fd25c1814f5bb679
Artur Wiśniewski <artixbtw@gmail.com>
2024-02-07 19:14:28 +0100

    add getAllReviews function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,1 +1,40 @@
-exports.getAllReviews = (req, res, next) => {};
+const Review = require("../models/reviewModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllReviews = async (req, res, next) => {
+  try {
+    const reviews = await Review.find();
+
+    res.status(200).json({
+      status: "success",
+      results: reviews.length,
+      data: {
+        reviews,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) {
+    req.body.tour = req.params.tourId;
+  }
+
+  if (!req.body.user) {
+    req.body.user = req.user.id;
+  }
+
+  const newReview = await Review.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      review: newReview,
+    },
+  });
+});
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -4,5 +4,6 @@ const reviewController = require("../controllers/reviewController");
 const router = express.Router();
 
 router.get("/", reviewController.getAllReviews);
+router.post("/", reviewController.createReview);
 
 module.exports = router;
