99783f2bf05cdbe176435d9cddbd1e7668f4a9c4
Artur Wiśniewski <artixbtw@gmail.com>
2024-03-19 16:56:51 +0100

    add pagination

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -4,6 +4,7 @@ import { API } from "../utils/api";
 import hero from "/hero.avif";
 import Select from "react-select";
 import { sortingStyles } from "../utils/sortingStyles";
+import ReactPaginate from "react-paginate";
 
 type Course = {
   title: string;
@@ -27,6 +28,7 @@ export const Courses = () => {
     const fetchCourses = async () => {
       try {
         const response = await API.get(
+          // `/skillsphere/courses?sort=${sortOption.value}&page=1&limit=6`,
           `/skillsphere/courses?sort=${sortOption.value}`,
         );
         setCourses(response.data.data.courses);
@@ -37,6 +39,26 @@ export const Courses = () => {
 
     fetchCourses();
   }, [sortOption]);
+
+  // ------------------------------------------------> Pagination
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const itemPerPage = 5;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemPerPage;
+    setCurrentItems(courses.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(courses.length / itemPerPage));
+  }, [itemOffset, courses, itemPerPage]);
+
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemPerPage) % courses.length;
+    setItemOffset(newOffset);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  // ------------------------------------------------> Pagination
 
   return (
     <div>
@@ -54,21 +76,40 @@ export const Courses = () => {
         </div>
         <div className="h-full w-full rounded-xl bg-black opacity-70"></div>
       </div>
-      <div className="flex gap-x-4">
-        <div className="w-1/4 py-2">
-          <Select
-            defaultValue={sortOption}
-            onChange={(selectedOption) =>
-              selectedOption && setSortOption(selectedOption)
-            }
-            options={options}
-            styles={sortingStyles}
-          />
-        </div>
-        <div className="w-3/4">
-          {courses.map((courseData: Course) => (
-            <Course key={courseData.id} courseData={courseData} />
-          ))}
+      <div>
+        <div className="flex gap-x-4">
+          <div className="w-1/4 py-2">
+            <Select
+              defaultValue={sortOption}
+              onChange={(selectedOption) =>
+                selectedOption && setSortOption(selectedOption)
+              }
+              options={options}
+              styles={sortingStyles}
+            />
+          </div>
+          <div className="w-3/4">
+            {currentItems.map((courseData: Course) => (
+              <Course key={courseData.id} courseData={courseData} />
+            ))}
+            <div>
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={2}
+                pageCount={pageCount}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+                className="mt-6 flex items-center justify-center space-x-4 text-lg"
+                containerClassName="bg-red-500"
+                pageLinkClassName="hover:text-yellow-400 duration-150"
+                activeLinkClassName="text-yellow-500 underline duration-150"
+                nextClassName="py-2 px-4 hover:text-yellow-400 active:text-yellow-500 duration-150"
+                previousClassName="py-2 px-4 hover:text-yellow-400 active:text-yellow-500 duration-150"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
