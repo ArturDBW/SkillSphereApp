0a0447eee7ca7f0b1e47d4d6870e002d294edb6b
Artur Wiśniewski <artixbtw@gmail.com>
2024-03-20 12:31:18 +0100

    add logic to searchbar

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -22,8 +22,16 @@ const options = [
 ];
 
 export const Courses = () => {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState<Course[]>([]);
+  // Sorting state
   const [sortOption, setSortOption] = useState(options[0]);
+  // Search state
+  const [searchTerm, setSearchTerm] = useState("");
+  // Pagination state
+  const [currentItems, setCurrentItems] = useState<Course[]>([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const itemPerPage = 5;
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -42,26 +50,36 @@ export const Courses = () => {
 
   // ------------------------------------------------> Pagination
 
-  const [currentItems, setCurrentItems] = useState([]);
-  const [itemOffset, setItemOffset] = useState(0);
-  const [pageCount, setPageCount] = useState(0);
-  const itemPerPage = 5;
-
   useEffect(() => {
     const endOffset = itemOffset + itemPerPage;
-    setCurrentItems(courses.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(courses.length / itemPerPage));
-  }, [itemOffset, courses, itemPerPage]);
+    setCurrentItems(
+      courses
+        .filter((course) =>
+          course.title.toLowerCase().includes(searchTerm.toLowerCase()),
+        )
+        .slice(itemOffset, endOffset),
+    );
+    setPageCount(
+      Math.ceil(
+        courses.filter((course) =>
+          course.title.toLowerCase().includes(searchTerm.toLowerCase()),
+        ).length / itemPerPage,
+      ),
+    );
+  }, [courses, itemOffset, searchTerm]);
 
   const handlePageClick = (event: { selected: number }) => {
-    const newOffset = (event.selected * itemPerPage) % courses.length;
+    const newOffset =
+      (event.selected * itemPerPage) %
+      courses.filter((course) =>
+        course.title.toLowerCase().includes(searchTerm.toLowerCase()),
+      ).length;
     setItemOffset(newOffset);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-  // ------------------------------------------------> Pagination
 
   return (
-    <div>
+    <div className="h-[calc(100vh-72px)]">
       <div
         className="relative mb-5 mt-10 h-[200px] rounded-xl bg-cover bg-center bg-no-repeat"
         style={{
@@ -91,6 +109,8 @@ export const Courses = () => {
           <div className="w-3/4">
             <div className="relative flex justify-between">
               <input
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 type="text"
                 placeholder="Search courses..."
                 className="h-[38px] w-full rounded-md border border-[#ccc] px-10 outline-none hover:border-yellow-500 focus:border-2 focus:border-yellow-500"
@@ -100,7 +120,14 @@ export const Courses = () => {
                 className="absolute left-2 top-2 text-[#ccc]"
               />
               <span className="flex w-32 items-end justify-end text-lg font-bold">
-                Results: {courses.length}
+                Results:{" "}
+                {
+                  courses.filter((course) =>
+                    course.title
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase()),
+                  ).length
+                }
               </span>
             </div>
             {currentItems.map((courseData: Course) => (
